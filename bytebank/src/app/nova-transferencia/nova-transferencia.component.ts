beed7a32513b4ebe133411dd2e3aa57e9af6a80c
MarcoAntonioBorges <mabo.marcoantonio@gmail.com>
2022-01-11 17:20:17 -0300

    First Application 4 road Expert Angular level

---
+++ bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,40 @@
+import { Transferencia } from './../models/transferencia.model';
+import { TransferenciaService } from './../services/transferencia.service';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+export class NovaTransferenciaComponent {
+
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number
+  destino: number
+
+  constructor(private service: TransferenciaService, private router: Router) {}
+
+  transferir() {
+    const valorEmitir: Transferencia = {valor: this.valor, destino: this.destino};
+
+    this.service.adicionar(valorEmitir).subscribe({
+      next: (v) => {
+        console.log(v);
+        this.limparCampos();
+        this.router.navigateByUrl('extrato');
+      },
+      error: (e) => console.error(e),
+    });
+
+  }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
+
+}
